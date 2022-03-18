a9066161d69c30af64892150338bfe9c83e5cd9a
Aleksandr Nazarov <nazarov.alexander2013@gmail.com>
2022-03-18 14:40:49 +0300

    Splash screen added

--- app/src/main/java/com/mytestprogram/composenavigaiton/Navigation.kt
+++ app/src/main/java/com/mytestprogram/composenavigaiton/Navigation.kt
@@ -1,5 +1,8 @@
 package com.mytestprogram.composenavigaiton
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -7,6 +10,8 @@ import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavType
@@ -14,11 +19,15 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import kotlinx.coroutines.delay
 
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = Screen.MainScreen.route) {
+    NavHost(navController = navController, startDestination = Screen.SplashScreen.route) {
+        composable(route = Screen.SplashScreen.route) {
+            SplashScreen(navController = navController)
+        }
         composable(route = Screen.MainScreen.route) {
             MainScreen(navController = navController)
         }
@@ -37,6 +46,32 @@ fun Navigation() {
     }
 }
 
+@Composable
+fun SplashScreen(navController: NavController) {
+    val scale = remember {
+        androidx.compose.animation.core.Animatable(0f)
+    }
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(
+                durationMillis = 1000,
+                easing = {
+                    OvershootInterpolator(2f).getInterpolation(it)
+                }
+            )
+        )
+        delay(3000L)
+        navController.navigate(Screen.MainScreen.route)
+    }
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Logo",
+            modifier = Modifier.scale(scale.value)
+        )
+    }
+}
 @Composable
 fun MainScreen(navController: NavController) {
     var text by remember {
--- app/src/main/java/com/mytestprogram/composenavigaiton/Screen.kt
+++ app/src/main/java/com/mytestprogram/composenavigaiton/Screen.kt
@@ -3,6 +3,7 @@ package com.mytestprogram.composenavigaiton
 sealed class Screen(val route: String) {
     object MainScreen: Screen("main_screen")
     object DetailScreen: Screen("detail_screen")
+    object SplashScreen: Screen("splash_screen")
 
     fun withArgs(vararg args: String): String {
         return buildString {
